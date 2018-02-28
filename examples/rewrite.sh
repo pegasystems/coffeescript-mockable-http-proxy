b540b8a5e90e21eb48d6473e9be02c214a566a64
siept <Tomasz.Sieprawski@pega.com>
2018-02-28 15:35:29 +0100

    Fixed usage of jq in example.

--- examples/rewrite.sh
+++ examples/rewrite.sh
@@ -16,18 +16,18 @@ pkill -f main.coffee
 pkill -f http-server
 
 ../node_modules/.bin/coffee ../main.coffee &
-wget -q -O - --retry-connrefused http://localhost:31338/ || exit 1
+wget -q -O /dev/null --retry-connrefused http://localhost:31338/ || exit 1
 
 http-server -p 31339 &
-wget -q -O - --retry-connrefused http://localhost:31339/ || exit 1
+wget -q -O /dev/null --retry-connrefused http://localhost:31339/ || exit 1
 
 # 1. Forward all requests
 
 cat << EOF > postdata.txt
 {"path":"","priority":10,"forward":{"host":"127.0.0.1","port":31339}}
 EOF
-wget -q -O - http://127.0.0.1:31338/routes --header 'Content-Type: application/json' --post-file=postdata.txt | jq || exit 1
-wget -q -O - http://127.0.0.1:31338/routes | jq || exit 1
+wget -q -O - http://127.0.0.1:31338/routes --header 'Content-Type: application/json' --post-file=postdata.txt | jq . || exit 1
+wget -q -O - http://127.0.0.1:31338/routes | jq . || exit 1
 
 wget -q -O post http://127.0.0.1:31337/ || exit 1
 diff index.html post || exit 1
@@ -38,9 +38,9 @@ rm post
 cat << EOF > postdata.txt
 {"path":"","priority":999,"log": true,"times":1,"response":{"code":"200","body": "krowa"}}
 EOF
-wget -q -O - http://127.0.0.1:31338/routes --header 'Content-Type: application/json' --post-file=postdata.txt | jq || exit 1
-wget -q -O - http://127.0.0.1:31338/routes | jq || exit 1
-wget -q -O - http://127.0.0.1:31338/logs | jq || exit 1
+wget -q -O - http://127.0.0.1:31338/routes --header 'Content-Type: application/json' --post-file=postdata.txt | jq . || exit 1
+wget -q -O - http://127.0.0.1:31338/routes | jq . || exit 1
+wget -q -O - http://127.0.0.1:31338/logs | jq . || exit 1
 
 # 2.1. Get logged request
 
@@ -53,7 +53,7 @@ wget -O - http://127.0.0.1:31338/logs > post || exit 1
 echo -n '[]' > pre
 diff -u pre post && exit 1
 LOGID=$(jq -r '.[0]' post)
-wget -O - "http://127.0.0.1:31338/log/${LOGID}" | jq || exit 1
+wget -O - "http://127.0.0.1:31338/log/${LOGID}" | jq . || exit 1
 rm pre post
 
 # 2.2. Second request is not rewrited
